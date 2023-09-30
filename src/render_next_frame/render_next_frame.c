cf25b2a9b3fc7ad4d69000b968d0c2e503b55db8
Diogo Dantas goncalves <ddantas-@c2r4s1.42lisboa.com>
2023-09-30 17:46:40 +0100

    load_colors done

--- src/render_next_frame/render_next_frame.c
+++ src/render_next_frame/render_next_frame.c
@@ -2,6 +2,22 @@
 
 int	render_next_frame(t_cubed *cubed)
 {
+	// int w;
+	// int h;
+
+
+	// /* draw ceiling */
+	// w = 0;
+	// while (w < WIN_W / 2)
+	// {
+	// 	h = 0;
+	// 	while (h < WIN_H / 2)
+	// 	{
+			
+	// 		h++;
+	// 	}
+	// 	w++;
+	// }
 	(void)cubed;
 	return (0);
 }
