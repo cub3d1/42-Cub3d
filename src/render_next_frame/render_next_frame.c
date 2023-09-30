d4ed12baac83ae90bc087ed2186ec5135af8ec4a
Diogo Dantas goncalves <ddantas-@c2r4s1.42lisboa.com>
2023-09-30 18:00:54 +0100

    rendering floor and ceiling

--- src/render_next_frame/render_next_frame.c
+++ src/render_next_frame/render_next_frame.c
@@ -1,23 +1,44 @@
 #include "../../include/cub3d.h"
 
+int	create_trgb(unsigned char t, unsigned char r, unsigned char g, unsigned char b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
 int	render_next_frame(t_cubed *cubed)
 {
-	// int w;
-	// int h;
+	int w;
+	int h;
+	int color;
 
-
-	// /* draw ceiling */
-	// w = 0;
-	// while (w < WIN_W / 2)
-	// {
-	// 	h = 0;
-	// 	while (h < WIN_H / 2)
-	// 	{
-			
-	// 		h++;
-	// 	}
-	// 	w++;
-	// }
+	/* draw ceiling */
+	w = 0;
+	while (w < WIN_W)
+	{
+		h = 0;
+		while (h < WIN_H / 2)
+		{
+			color = create_trgb(0, cubed->mlx->ceiling_color[0], \
+				cubed->mlx->ceiling_color[1], cubed->mlx->ceiling_color[2]);
+			mlx_pixel_put(cubed->mlx->mlx_ptr, cubed->mlx->win_ptr, w, h, color);
+			h++;
+		}
+		w++;
+	}
+	/* draw floor */
+	w = 0;
+	while (w < WIN_W)
+	{
+		h = WIN_H / 2;
+		while (h < WIN_H)
+		{
+			color = create_trgb(0, cubed->mlx->floor_color[0], \
+				cubed->mlx->floor_color[1], cubed->mlx->floor_color[2]);
+			mlx_pixel_put(cubed->mlx->mlx_ptr, cubed->mlx->win_ptr, w, h, color);
+			h++;
+		}
+		w++;
+	}
 	(void)cubed;
 	return (0);
 }
