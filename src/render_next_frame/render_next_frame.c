91c5441451d70b8eb088e3b9953549a9f9188527
Diogo G <desktop-diogo>
2023-10-01 21:40:42 +0100

    commented debug messages

--- src/render_next_frame/render_next_frame.c
+++ src/render_next_frame/render_next_frame.c
@@ -64,17 +64,17 @@ void	print_info(t_cubed *cubed, int frame_counter)
 	// frame = ft_itoa(frame_counter);
 	// ft_printf_fd(1, "frame = %s | ", frame);
 	// free(frame);
-	printf("p.pos_x = %f | ", cubed->player->pos_x);
-	printf("p.pos_y = %f | ", cubed->player->pos_y);
-	printf("p.pos_x_array = %f | ", cubed->player->pos_x_array);
-	printf("p.pos_y_array = %f | ", cubed->player->pos_y_array);
+	// printf("p.pos_x = %f | ", cubed->player->pos_x);
+	// printf("p.pos_y = %f | ", cubed->player->pos_y);
+	// printf("p.pos_x_array = %f | ", cubed->player->pos_x_array);
+	// printf("p.pos_y_array = %f | ", cubed->player->pos_y_array);
 	// printf("show_minimap = %d | ", cubed->keys->show_minimap);
 	// printf("w = %d | ", cubed->keys->w);
 	// printf("a = %d | ", cubed->keys->a);
 	// printf("s = %d | ", cubed->keys->s);
 	// printf("d = %d | ", cubed->keys->d);
 	// printf("angle = %f | ", cubed->player->angle);
-	printf("\n");
+	// printf("\n");
 	(void)cubed; (void)frame_counter;
 }
 
