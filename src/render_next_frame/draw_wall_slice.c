f5964f2f59ee4e18f6be5bab98ae1ba8e3866873
fmouronh <fmouronh@student.42lisboa.com>
2024-01-28 12:28:06 -0600

    working on textures

--- src/render_next_frame/draw_wall_slice.c
+++ src/render_next_frame/draw_wall_slice.c
@@ -33,15 +33,16 @@ static void	copy_to_buffer(t_our_img *texture, unsigned int *buffer, \
 	}
 }
 
-static int	slice_on_y(int render_h, int texture_h)
+static int	slice_on_y(int render_h, int texture_h, t_ray *ray)
 {
-	int		render_start;
-	double	height_ratio;
+//	int		render_start;
+//	double	height_ratio;
 
-	render_start = (render_h / 2) - (WIN_H / 2);
+	(void)texture_h;
+//	render_start = (render_h / 2) - (WIN_H / 2);
 //	printf("render_h: %d\nrender_start: %d\n", render_h, render_start);
-	height_ratio = (double)render_start / (double)render_h;
-	return ((int)((double)texture_h * height_ratio));
+//	height_ratio = (double)render_start / (double)render_h;
+	return ((render_h - WIN_H) / 2 * ray->render_step);
 }
 
 static void	copy_to_canvas(t_canvas *canvas, unsigned int *buffer, 
@@ -74,13 +75,13 @@ void	pre_render_slice(t_canvas *canvas, t_our_img *texture, t_ray *ray)
 	copy_size = ray->render_h;
 	ray->texture_y = 0;
 //	printf("ray->render_h: %d\n", ray->render_h);
+	ray->render_step = 1.0 * (double)texture->h / (double)ray->render_h;
 	if (copy_size > WIN_H)
 	{
 		copy_size = WIN_H;
-		ray->texture_y = slice_on_y(ray->render_h, texture->h);
+		ray->texture_y = slice_on_y(ray->render_h, texture->h, ray);
 	}
 //	printf("ray->texture_y: %d\n", ray->texture_y);
-	ray->render_step = (double)texture->h / (double)ray->render_h;
 	copy_to_buffer(texture, canvas->buffer_array[copy_size - 1], \
 					ray, copy_size);
 	ray->canvas_start = (WIN_H - 1) / 2 - copy_size / 2;
